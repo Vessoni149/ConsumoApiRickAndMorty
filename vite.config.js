aaf61d0015aaf4c63ba1925e646b9017d4f99978
Agustin Vessoni <agustinvessoni149@gmail.com>
2023-02-02 20:39:01 -0600

    commit 1

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://Vessoni149.github.io/ConsumoApiRickAndMorty/",
+  plugins: [react()],
+})
