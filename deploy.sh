61944faf1bfe13dbb4cce9d8e0855cc75a6d85ab
Agustin Vessoni <agustinvessoni149@gmail.com>
2023-02-02 20:40:40 -0600

    commit2

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:vessoni149/ConsumoApiRickAndMorty.git main:gh-pages
+git push -f git@github.com:Vessoni149/ConsumoApiRickAndMorty.git main:gh-pages
 
 cd -
